93ad1afad859c2af98751598fd6d80fab3e0f234
Kamil T <trzaskomakamil@gmail.com>
2019-06-14 23:41:39 +0200

    few variables added to better control elements

--- Game/script.js
+++ Game/script.js
@@ -1,12 +1,13 @@
 const SIZE = 30
 const SPACE_BETWEEN = SIZE + SIZE/2
-const FREE_SPACE = (SIZE * 10 + 9 * SPACE_BETWEEN) / 2 
+let numOfEnemies = 15
+const FREE_SPACE = (40*SIZE - (SIZE + (numOfEnemies - 1) * SPACE_BETWEEN)) / 2 
 
 const body = document.querySelector('body')
 const canvas = document.createElement('canvas')
 body.append(canvas)
 
-canvas.setAttribute('width', `${45 * SIZE}px`)
+canvas.setAttribute('width', `${40 * SIZE}px`)
 canvas.setAttribute('height', `${30 * SIZE}px`)
 
 const ctx = canvas.getContext('2d')
@@ -17,14 +18,14 @@ function drawBurger (x, y, width, height, color = 'black') {
 }
 
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < numOfEnemies; i++) {
   drawBurger(i * SPACE_BETWEEN + FREE_SPACE, SIZE, SIZE, SIZE)
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < numOfEnemies; i++) {
   drawBurger(i * SPACE_BETWEEN + FREE_SPACE , SIZE + SPACE_BETWEEN, SIZE, SIZE)
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < numOfEnemies; i++) {
   drawBurger(i * SPACE_BETWEEN + FREE_SPACE, SIZE + 2 * SPACE_BETWEEN, SIZE, SIZE)
 }
