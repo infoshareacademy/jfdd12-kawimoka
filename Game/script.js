c6295dc156049d9afb844dbdc41121cac4b62f67
Kamil T <trzaskomakamil@gmail.com>
2019-06-17 12:13:51 +0200

    Pretier all files

--- Game/script.js
+++ Game/script.js
@@ -1,62 +1,58 @@
 const SIZE = 30
-const SPACE_BETWEEN = SIZE + SIZE/2
-let numOfEnemies = 15
-const FREE_SPACE = (40*SIZE - (SIZE + (numOfEnemies - 1) * SPACE_BETWEEN)) / 2 
+const SPACE_BETWEEN = SIZE + SIZE / 2
+let numOfBurgers = 15
 const WIDTH = 900
 const HEIGHT = 600
 const INSTRUCTION_WIDTH = 300
 const INSTRUCTION_HEIGHT = 300
 const BOY_WIDTH = 100
 const BOY_HEIGHT = 100
+const FREE_SPACE = (WIDTH - (SIZE + (numOfBurgers - 1) * SPACE_BETWEEN)) / 2
 
 let boy = {
   x: WIDTH / 2,
-  y: (HEIGHT - BOY_HEIGHT) - 50,
+  y: HEIGHT - BOY_HEIGHT - 50
 }
 
 const body = document.querySelector('body')
 const canvas = document.createElement('canvas')
 body.append(canvas)
 
-canvas.setAttribute('width', `${30 * SIZE}px`)
-canvas.setAttribute('height', `${20 * SIZE}px`)
+canvas.setAttribute('width', WIDTH)
+canvas.setAttribute('height', HEIGHT)
 const ctx = canvas.getContext('2d')
 
-let isPlaying = false;
+let isPlaying = false
 
 function animate() {
   drawGame()
   requestAnimationFrame(animate)
 }
-animate();
+animate()
 
-drawPlayButton();
+drawPlayButton()
 
-
-function drawGame(){
-  drawBackground();
-  drawBurgers();
-  drawBoy();
-  if(!isPlaying){
-    drawInstruction();
+function drawGame() {
+  drawBackground()
+  drawBurgers()
+  drawBoy()
+  if (!isPlaying) {
+    drawInstruction()
   }
 }
 
-
 function drawBurgers() {
-
-  for (let i = 0; i < numOfEnemies; i++) {
+  for (let i = 0; i < numOfBurgers; i++) {
     drawBurger(i * SPACE_BETWEEN + FREE_SPACE, SIZE, SIZE, SIZE)
   }
-  
-  for (let i = 0; i < numOfEnemies; i++) {
-    drawBurger(i * SPACE_BETWEEN + FREE_SPACE , SIZE + SPACE_BETWEEN, SIZE, SIZE)
-  }
-  
-  for (let i = 0; i < numOfEnemies; i++) {
-    drawBurger(i * SPACE_BETWEEN + FREE_SPACE, SIZE + 2 * SPACE_BETWEEN, SIZE, SIZE)
+
+  for (let i = 0; i < numOfBurgers; i++) {
+    drawBurger(i * SPACE_BETWEEN + FREE_SPACE, SIZE + SPACE_BETWEEN, SIZE, SIZE)
   }
 
+  for (let i = 0; i < numOfBurgers; i++) {
+    drawBurger(i * SPACE_BETWEEN + FREE_SPACE, SIZE + 2 * SPACE_BETWEEN, SIZE, SIZE)
+  }
 }
 
 function drawBackground() {
@@ -64,7 +60,13 @@ function drawBackground() {
 }
 
 function drawInstruction() {
-  drawImage('game-images/instruction.png', (WIDTH - INSTRUCTION_WIDTH)/2, (HEIGHT - INSTRUCTION_HEIGHT)/2, INSTRUCTION_WIDTH, INSTRUCTION_HEIGHT)
+  drawImage(
+    'game-images/instruction.png',
+    (WIDTH - INSTRUCTION_WIDTH) / 2,
+    (HEIGHT - INSTRUCTION_HEIGHT) / 2,
+    INSTRUCTION_WIDTH,
+    INSTRUCTION_HEIGHT
+  )
 }
 
 function drawBoy() {
@@ -73,20 +75,19 @@ function drawBoy() {
 
 function drawPlayButton() {
   var button = document.createElement('button')
-  button.innerHTML = "PLAY"
-  button.classList.add("button-style");
-  button.addEventListener('click', function(){
-    isPlaying = !isPlaying;
+  button.innerHTML = 'PLAY'
+  button.classList.add('button-style')
+  button.addEventListener('click', function() {
+    isPlaying = !isPlaying
   })
-  body.append(button);
+  body.append(button)
 }
 
-
-function drawBurger (x, y, width, height) {
-  drawImage('game-images/burger.png',x, y, width, height)
+function drawBurger(x, y, width, height) {
+  drawImage('game-images/burger.png', x, y, width, height)
 }
 
-function drawImage (imageUrl, x, y, w, h, onload = () => {}) {
+function drawImage(imageUrl, x, y, w, h, onload = () => {}) {
   const image = new Image()
   image.src = imageUrl
   image.onload = function() {
