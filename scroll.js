0ac09e977beb8aaf1a14a14ee86d1f06bc68f8d2
Kamil T <trzaskomakamil@gmail.com>
2019-06-30 14:10:38 +0200

    return to top button added

--- scroll.js
+++ scroll.js
@@ -1,19 +1,23 @@
 let isTeamWasShown = false
 
-window.addEventListener('scroll',function(){
-    let yScrollPosition = window.pageYOffset
-    let elementPosition = document.getElementById("team-nav").offsetTop
-    let visualScreenHeight = window.innerHeight;
-    let activationOffset = 0.85;
-    let heightConditionForAnimate = yScrollPosition + activationOffset * visualScreenHeight >= elementPosition
+window.addEventListener('scroll', function() {
+  let yScrollPosition = window.pageYOffset
+  let elementPosition = document.getElementById('team-nav').offsetTop
+  let visualScreenHeight = window.innerHeight
+  let activationOffset = 0.85
+  let heightConditionForAnimate =
+    yScrollPosition + activationOffset * visualScreenHeight >= elementPosition
 
-    if (!isTeamWasShown && heightConditionForAnimate) {
-        isTeamWasShown = true
-        let arrayOfImages = document.querySelectorAll(".animated")
-        arrayOfImages.forEach((imageEl)=>{
-            imageEl.classList.toggle("hide-pictures")
-            imageEl.classList.toggle("flipInX")
-        })
-    }
-    
+  if (!isTeamWasShown && heightConditionForAnimate) {
+    isTeamWasShown = true
+    let arrayOfImages = document.querySelectorAll('.animated')
+    arrayOfImages.forEach(imageEl => {
+      imageEl.classList.toggle('hide-pictures')
+      imageEl.classList.toggle('flipInX')
+    })
+  }
 })
+
+function topFunction() {
+  document.documentElement.scrollTop = 0
+}
